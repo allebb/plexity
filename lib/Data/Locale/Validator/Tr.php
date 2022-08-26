ca6dad2537692a7dea77135c17825f9cc3218ee0
Mehmet Ali <devteam@gelistir.org>
2022-08-26 12:53:20 +0300

    add, Turkish message list class has been created.

---
+++ lib/Data/Locale/Validator/Tr.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ballen\Plexity\Data\Locale\Validator;
+
+class Tr{
+    static $messages = [
+        'checkMinimumLength'=>'Uzunluk, minimum uzunluk gereksinimlerini karşılamıyor.',
+        'checkMaximumLength'=>'Uzunluk, maksimum uzunluk gereksinimlerini aşıyor.',
+        'checkLowerCase'=>'Dize, küçük harf gereksinimlerini karşılayamadı.',
+        'checkUpperCase'=>'Dize, büyük harf gereksinimlerini karşılayamadı.',
+        'checkNumericCharacters'=>'Dize sayısal karakter gereksinimlerini karşılayamadı.',
+        'checkSpecialCharacters'=>'Dize, özel karakter gereksinimlerini karşılayamadı.',
+        'validateNotInArray'=>'Dize, izin verilmeyen değerler gereksinimleri listesinde var.',
+        'validateNotInPasswordHistoryImplementation'=>'Dize, izin verilmeyen değerler gereksinimleri listesinde var.'
+    ];
+}
